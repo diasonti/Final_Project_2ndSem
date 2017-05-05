9e0605e5db99d6942259a1308e7b633558bbdb9c
Vladimir <diasonti@gmail.com>
2017-05-05 20:07:01 +0600

    .

---
+++ src/CashierClient/App.java
@@ -0,0 +1,14 @@
+package CashierClient;
+
+/**
+ * Created by Vladimir on 05.05.2017.
+ */
+class App {
+
+    public static void main(String[] args){
+
+        Frame frame = new Frame();
+
+    }
+
+}
---
+++ src/CashierClient/Frame.java
@@ -0,0 +1,18 @@
+package CashierClient;
+
+import javax.swing.*;
+
+/**
+ * Created by Vladimir on 05.05.2017.
+ */
+class Frame extends JFrame {
+
+    public Frame(){
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 600);
+        setVisible(true);
+
+    }
+
+}
